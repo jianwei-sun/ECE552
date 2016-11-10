7e4eef54fda0227305c8b067ccc837f44610f09c
Sun  Jianwei <sunjianw@ug222.eecg>
2016-11-10 13:05:28 -0500

    Updated the microbenchmark with inline assembly

--- lab4/simplesim-3.0d-assig4/mbq1.c
+++ lab4/simplesim-3.0d-assig4/mbq1.c
@@ -6,10 +6,18 @@
 //Microbenchmark program to test for reliability of next line prefetching
 
 int main(void){
-	register int i, b;
+	asm("move	$3,$sp");
+	asm("$L101:	slt	$2,$3,40000");
+	asm("bne	$2,$0,$L1");
+	asm("lw		$4,0($3)");
+	asm("addu	$3,$3,4");
+	asm("j	$L101");
+
+/*	register int i, b;
 	for(i = 0; i < ITERATIONS; i++){
 		b += i;
 		asm("addu	$4,$4,$3");
 	} 
 	return 0;
+*/
 } 
--- lab4/simplesim-3.0d-assig4/mbq1.s
+++ lab4/simplesim-3.0d-assig4/mbq1.s
@@ -27,22 +27,14 @@ main:
 	sw	$fp,16($sp)
 	move	$fp,$sp
 	jal	__main
-	move	$3,$0
-$L2:
-	slt	$2,$3,100
-	bne	$2,$0,$L5
-	j	$L3
-$L5:
-	addu	$4,$4,$3
  #APP
-	addu	$4,$4,$3
+	move	$3,$sp
+	$L101:	slt	$2,$3,40000
+	bne	$2,$0,$L1
+	lw		$4,0($3)
+	addu	$3,$3,4
+	j	$L101
  #NO_APP
-$L4:
-	addu	$3,$3,1
-	j	$L2
-$L3:
-	move	$2,$0
-	j	$L1
 $L1:
 	move	$sp,$fp			# sp not trusted here
 	lw	$31,20($sp)
