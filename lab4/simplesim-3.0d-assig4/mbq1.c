dccc210e6e68ff77c3d1d7873b992483e145f06c
Zhang  Yi Fan <zhang532@ug159.eecg>
2016-11-19 16:54:37 -0500

    modified mbq1.c

--- lab4/simplesim-3.0d-assig4/mbq1.c
+++ lab4/simplesim-3.0d-assig4/mbq1.c
@@ -6,18 +6,20 @@
 //Microbenchmark program to test for reliability of next line prefetching
 
 int main(void){
+		
 	asm("move	$3,$sp");
-	asm("$L101:	slt	$2,$3,40000");
-	asm("bne	$2,$0,$L1");
+	//asm("move	$3, $0");
+	asm("addu	$5, $sp, 4000000");
+	asm("$L101:	sltu	$2,$3,$5");
+	asm("beq	$2,$0,$L1");
 	asm("lw		$4,0($3)");
 	asm("addu	$3,$3,4");
 	asm("j	$L101");
-
-/*	register int i, b;
+/*
+	register int i, b;
 	for(i = 0; i < ITERATIONS; i++){
 		b += i;
 		asm("addu	$4,$4,$3");
 	} 
-	return 0;
-*/
-} 
+	return 0; */
+}
